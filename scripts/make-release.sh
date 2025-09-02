258664ad5122aafcca809acd897ada2ee3a0e5d4
Alexander Koller <akoller@gmail.com>
2025-09-02 21:11:31 +0200

    finetuned release process

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -9,30 +9,35 @@ then
 fi
 
 
+# Prepare release directory
 RELEASE_DIR=release/preview/pergamon/$VERSION
 DOCS_DIR=docs
 
-# Put together release
 rm -rf $RELEASE_DIR
 mkdir -p $RELEASE_DIR
 
-cp lib.typ $RELEASE_DIR/lib.typ
-cp -r src $RELEASE_DIR
-cp README.md $RELEASE_DIR/
-cp LICENSE $RELEASE_DIR/
 
-# replace version in typst.toml
+# Update typst.toml
 sed -i '.bak' "s/^version = \".*\"/version = \"$VERSION\"/" typst.toml
 # -i .bak is Mac-specific
 cp typst.toml $RELEASE_DIR
-# old: sed "s/VERSION/$VERSION/g" typst-template.toml > $RELEASE_DIR/typst.toml
 
 
-# Generate up-to-date documentation
+# Generate up-to-date documentation (using new version)
 typst compile pergamon.typ
 mkdir -p $DOCS_DIR
 cp pergamon.pdf $DOCS_DIR/pergamon-$VERSION.pdf
 cp $DOCS_DIR/pergamon-$VERSION.pdf $DOCS_DIR/pergamon-latest.pdf
+
+
+# Update occurrences of version in the README
+sed -i '.bak' -e "s/pergamon-.*.pdf/pergamon-$VERSION.pdf/" -e "s/preview\/pergamon:[^\"]*/preview\/pergamon:$VERSION/" README.md
+
+# Put together release
+cp lib.typ $RELEASE_DIR/lib.typ
+cp -r src $RELEASE_DIR
+cp README.md $RELEASE_DIR/
+cp LICENSE $RELEASE_DIR/
 
 
 echo "Package is ready for release in $RELEASE_DIR."
